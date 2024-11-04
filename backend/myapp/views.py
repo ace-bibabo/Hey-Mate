a7796cc5458d128cef1ee9dfa372068952088126
lili-coogee <z5441928@ad.unsw.edu.au>
2024-11-05 09:35:00 +1100

    ace/update

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
-from .models_bak import ChatBot, Prompt, PromptGroup
+from .models import ChatBot, Prompt, PromptGroup
 from .serializers import PromptSerializer, PromptGroupSerializer, question_schema, response_schema
 
 class ChatAPIView(APIView):
